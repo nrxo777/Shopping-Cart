ca54faac4b6ee62592046a3cf1a7c70ec9f70adb
NoorXO <noorxcalma@gmail.com>
2025-02-08 16:07:22 +0600

    empty_cart_msg

--- logic.js
+++ logic.js
@@ -1,8 +1,7 @@
 // ********** UI elements **********
 const all_item = document.querySelector('#all-item');
 const cart_list = document.querySelector('#cart-list');
-
-
+let empty_cart_message = document.querySelector('#emptyCartMessage');
 
 // ********** Event Listeners **********
 all_item.addEventListener('click', function(e) {
@@ -25,6 +24,7 @@ function add_to_list(button) {
     `;
 
     cart_list.appendChild(item_row);
+    empty_cart();    
 }
 
 // ********** Remove **********
@@ -32,12 +32,24 @@ function add_to_list(button) {
 
 cart_list.addEventListener('click', function(ri) {
     ri.preventDefault();
-    
+
     if (ri.target.classList.contains('remove') === true) {
         let remove_item = ri.target.parentElement.parentElement
         
         remove_item.remove()
+        empty_cart();
     }
 
 });
 
+// ********** Message if cart is Empty **********
+
+function empty_cart() {
+    if(cart_list.children.length === 0) {
+        empty_cart_message.innerText = "Your cart is empty.";
+    } else {
+        empty_cart_message.innerText = '';
+    }
+}
+
+empty_cart();
